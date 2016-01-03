c0b39122edba37060f29169b856c5d68ee974c4a
Pan-Masa <nasmac@nasmacs-macbook-air.local>
2016-01-03 20:07:16 +0700

    Initial Commit

---
+++ pokedex-by-pancht/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-pancht
+//
+//  Created by Nas@Mac on 3/1/59.
+//  Copyright © พ.ศ. 2559 Nas@Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
