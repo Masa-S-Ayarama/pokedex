11bda2ae930121a6a51551b2384aa97445604f73
Pan-Masa <nasmac@NasMacs-MacBook-Air.local>
2016-01-04 21:23:30 +0700

    added Pokemon detailVC & class

--- pokedex-by-pancht/PokemonDetailVC.swift
+++ pokedex-by-pancht/PokemonDetailVC.swift
@@ -12,6 +12,30 @@ class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
     
+    @IBOutlet weak var mainImg: UIImageView!
+    
+    @IBOutlet weak var descriptionLbl: UILabel!
+    
+    @IBOutlet weak var typeLbl: UILabel!
+    
+    @IBOutlet weak var defenseLbl: UILabel!
+    
+    @IBOutlet weak var heightLbl: UILabel!
+    
+    @IBOutlet weak var widthLbl: UILabel!
+    
+    @IBOutlet weak var pokedexLbl: UILabel!
+    
+    @IBOutlet weak var attackLbl: UILabel!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    
+    @IBOutlet weak var evoTextLbl: UILabel!
+    
+    
+    
     var pokemon: Pokemon!
 
     override func viewDidLoad() {
@@ -20,6 +44,10 @@ class PokemonDetailVC: UIViewController {
         nameLbl.text = pokemon.name
     }
 
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
